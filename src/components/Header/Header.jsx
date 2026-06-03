85923332be530554d3cba0c40d8fda422fb649bc
Endrius <endriussilvasantos@gmail.com>
2026-06-03 20:48:59 -0300

    criado componente Header e novas cores adicionadas no index.css

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <>
+      <header className="bg-surface font-[Anton] shadow-[0px_10px_30px_rgba(15,23,42,0.05)]">
+        <h1 className="text-primary font-bold tracking-wider uppercase text-3xl p-4">
+          MySticker
+        </h1>
+      </header>
+    </>
+  );
+};
+
+export default Header;
